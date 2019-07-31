7761fb0c886726a6b4fbd0e6714167650580aca0
GuHenry <guga_hmaia@outlook.com>
2019-07-31 11:23:14 -0300

    Terminado exemplo

---
+++ Estrutura.sql
@@ -0,0 +1,24 @@
+﻿DROP TABLE IF EXISTS habilidades
+DROP TABLE  IF EXISTS animais;
+CREATE TABLE animais(
+id INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100),
+extinto BIT
+);
+
+SELECT * FROM animais;
+--ALTER TABLE animais ADD peso DECIMAL(4,2) DEFAULT 0.0
+
+--UPDATE animais SET peso = 0;
+
+--ALTER TABLE animais DROP CONSTRAINT DF__animais__peso__276EDEB3;
+--ALTER TABLE animais DROP COLUMN peso;
+
+CREATE TABLE habilidades(
+id INT PRIMARY KEY IDENTITY(1,1),
+
+id_animal INT,
+FOREIGN KEY(id_animal) REFERENCES animais (id),
+
+nome VARCHAR(100)
+);
--- Exemplo00/Program.cs
+++ Exemplo00/Program.cs
@@ -10,6 +10,57 @@ namespace Exemplo00
     {
         static void Main(string[] args)
         {
+            SistemaContext contexto = new SistemaContext();
+
+            #region InserirAnimal
+            Animal animal2 = new Animal();
+            animal2.Nome = "TRex";
+            animal2.Extinto = true;
+
+            contexto.Animais.Add(animal2);
+            contexto.SaveChanges();
+            Console.WriteLine("Registro salvo com sucesso");
+            #endregion InserirAnimal
+
+            #region Apagar
+            //Animal animalApagar = contexto.Animais.Where(x => x.Nome == "Porco aranha").First();
+            //contexto.Animais.Remove(animalApagar);
+            //contexto.SaveChanges();
+            #endregion Apagar
+
+            #region Atualizar
+            //var zebra = contexto.Animais.Where(x => x.Id == 4).First();
+            //zebra.Nome = "zebrinha";
+            //contexto.SaveChanges();
+            #endregion Atualizar
+
+            #region Selecionar
+            List<Animal> animais = contexto.Animais.ToList();
+            foreach(Animal animal in animais)
+            {
+                Console.WriteLine($"{animal.Id} - {animal.Nome} - {animal.Extinto}");
+            }
+            #endregion Selecionar
+
+            #region InserirHabilidade
+            Habilidade habilidade = new Habilidade();
+            habilidade.IdAnimal = 1;
+            habilidade.Nome = "Invisibilidade";
+
+            contexto.Habilidades.Add(habilidade);
+            contexto.SaveChanges();
+
+            var habilidades = contexto.Habilidades.Include("Animal").ToList();
+
+            foreach (Habilidade habilidadeAux in habilidades)
+            {
+                Console.WriteLine(habilidade.Nome);
+            }
+            #endregion InserirHabilidade
+
+
+
+
         }
     }
 }
